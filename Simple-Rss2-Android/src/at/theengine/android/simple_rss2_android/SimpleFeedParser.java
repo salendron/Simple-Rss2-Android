ad4cad57a353cf825148aec79dab9b3725b1c0a8
Salendron <salendron@XPS.(none)>
2013-11-01 15:47:15 +0100

    added SSL Handling

--- Simple-Rss2-Android/src/at/theengine/android/simple_rss2_android/SimpleFeedParser.java
+++ Simple-Rss2-Android/src/at/theengine/android/simple_rss2_android/SimpleFeedParser.java
@@ -2,8 +2,18 @@ package at.theengine.android.simple_rss2_android;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public abstract class SimpleFeedParser implements FeedParser {
 
@@ -26,9 +36,58 @@ public abstract class SimpleFeedParser implements FeedParser {
 
     protected InputStream getInputStream() {
         try {
-            return feedUrl.openConnection().getInputStream();
+        	 HttpURLConnection conn = null;
+        	 if (feedUrl.getProtocol().toLowerCase().equals("https")) {
+    	        trustAllHosts();
+    	        HttpsURLConnection https = (HttpsURLConnection) feedUrl.openConnection();
+    	        https.setHostnameVerifier(DO_NOT_VERIFY);
+    	        conn = https;
+    	     } else {
+    	        conn = (HttpURLConnection) feedUrl.openConnection();
+    	     }
+        	
+        	 return conn.getInputStream();
+            //return feedUrl.openConnection().getInputStream();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    //trust ssl
+    // always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+          public boolean verify(String hostname, SSLSession session) {
+              return true;
+          }
+   };
+
+
+    private static void trustAllHosts() {
+	  // Create a trust manager that does not validate certificate chains
+	  TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+	        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+	                return new java.security.cert.X509Certificate[] {};
+	        }
+	
+			@Override
+			public void checkClientTrusted(X509Certificate[] arg0,
+					String arg1) throws CertificateException {
+			}
+	
+			@Override
+			public void checkServerTrusted(X509Certificate[] arg0,
+					String arg1) throws CertificateException {
+			}
+	  } };
+	
+	  // Install the all-trusting trust manager
+	  try {
+	          SSLContext sc = SSLContext.getInstance("TLS");
+	          sc.init(null, trustAllCerts, new java.security.SecureRandom());
+	          HttpsURLConnection
+	                          .setDefaultSSLSocketFactory(sc.getSocketFactory());
+	  } catch (Exception e) {
+	          e.printStackTrace();
+	  }
     }
 }
